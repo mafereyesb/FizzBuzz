46f26a17727fc51f97686f6972128781d0fa3cb2
Maria Fernanda Reyes <mariafreyes05@gmail.com>
2025-09-08 18:24:18 -0400

    refactored to extract helper method

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -7,7 +7,13 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i < 100; i++) {
+        doFizzBuzz();
+    }
+
+    private static void doFizzBuzz() {
+        int i = 1;
+        while (i < 100) {
+//        for (int i = 1; i < 100; i++) {
 
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
@@ -31,6 +37,7 @@ class FizzBuzz {
                 System.out.println(i);
 
             }
+            i++;
         }
     }
 }
